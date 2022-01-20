0d3d540510d50213f05ab211c2d395a09439a88a
Hagar Hassan <63251874+hagarazzawy@users.noreply.github.com>
2022-01-20 21:34:06 +0200

    Update testbase.java

--- src/test/java/base/testbase.java
+++ src/test/java/base/testbase.java
@@ -9,13 +9,6 @@ import io.restassured.path.xml.element.Node;
 
 public class testbase {
 	
-	@BeforeClass
-	public void setup() {
-		
-		RestAssured.baseURI="http://www.colourlovers.com/api";
-		RestAssured.basePath ="/patterns";
-	}
-	
 	public boolean areViewsGreaterThan (List<Node> list,int number)
 	{
 		int NumsViewValue=0;
