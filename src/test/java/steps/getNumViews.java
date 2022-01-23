116261857b21c2aea434704f8f3d6bb5685b842f
hagar hassan <hagar.abdelrahman@itworx.com>
2022-01-23 17:30:28 +0200

    getNumViews

--- src/test/java/steps/getNumViews.java
+++ src/test/java/steps/getNumViews.java
@@ -5,18 +5,19 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
+import java.util.List;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.restassured.internal.path.xml.NodeChildrenImpl;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class getNumViews {
 	Response RS;
 	RequestSpecification RQ;
-	NodeChildrenImpl numViews;
+	List<String> numOfViewsList;
 
 	@Given("I am a user")
 	public void i_am_a_user() {
@@ -35,16 +36,16 @@ public class getNumViews {
 	@Then("I can get the number of views for each pattern")
 	public void I_can_get_the_number_of_views_for_each_pattern() {
 
-		numViews = RS.then().extract().path("patterns.pattern.numViews");
+		numOfViewsList = RS.xmlPath().getList("patterns.pattern.numViews");
 
-		assertFalse(numViews.list().isEmpty());
+		assertFalse(numOfViewsList.isEmpty());
 	}
 
 	@And("the number of views is greater than {int}")
 	public void the_number_of_views_is_greater_than(int number) {
 
-		for (int i = 0; i < numViews.size(); i++) {
-			int Viewsnum = Integer.parseInt(RS.xmlPath().getString("patterns.pattern.numViews["+i+"]"));
+		for (int i = 0; i < numOfViewsList.size(); i++) {
+			int Viewsnum = Integer.parseInt(RS.xmlPath().getString("patterns.pattern.numViews[" + i + "]"));
 			assertTrue(Viewsnum > number);
 
 		}
